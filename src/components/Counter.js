cfc93aa4a08aa33446c6d182da7e08a15032efa2
unknown <adminizba@WKS-331000-0166.mf.gov.pl>
2020-05-13 14:57:36 +0200

    Adding Counter - step 2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,12 +5,32 @@ import "./Counter.css";
 class Counter extends Component {
   constructor(props) {
     super(props);
-    this.state = { eventDate: props.eventDate, currentDate: Date.now() };
+    this.state = {
+      eventDate: props.eventDate,
+      days: 0,
+      hours: 0,
+      mins: 0,
+      secs: 0,
+    };
   }
 
   tick() {
+    let diffrence = Math.abs(
+      Math.floor((this.props.eventDate.start - Date.now()) / 1000)
+    );
+    console.log(diffrence);
+    let days = Math.floor(diffrence / (24 * 60 * 60));
+    let secsLeft = diffrence - days * 24 * 60 * 60;
+    let hours = Math.floor(secsLeft / (60 * 60));
+    secsLeft = secsLeft - hours * 60 * 60;
+    let mins = Math.floor(secsLeft / 60);
+    secsLeft = secsLeft - mins * 60;
+    let secs = secsLeft;
     this.setState((state) => ({
-      currentDate: Date.now(),
+      days,
+      hours,
+      mins,
+      secs,
     }));
   }
 
@@ -23,7 +43,16 @@ class Counter extends Component {
   }
 
   render() {
-    return <div className="counter text-center">{this.state.currentDate}</div>;
+    console.log(this.state.days);
+
+    return (
+      <div className="counter text-center">
+        <p className="size6vw">Już za</p>
+        <p>
+          {` ${this.state.days} dni ${this.state.hours} h ${this.state.mins} min ${this.state.secs} s`}
+        </p>
+      </div>
+    );
   }
 }
 
